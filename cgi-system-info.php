13caaff2927602e3978466c441d54329dc323fb9
Cyrille37 <cyrille37@gmail.com>
2013-02-14 13:23:12 +0100

    typo

--- cgi-system-info.php
+++ cgi-system-info.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
   Plugin Name: CGI System Info
   Plugin URI: https://github.com/Cyrille37/cgi-system-info
@@ -35,13 +36,37 @@ if (!class_exists('CgiSystemInfo')) {
 	class CgiSystemInfo {
 
 		const PLUGIN = 'cgi-system-info';
-	const WP_ROLE = 'edit_pages';
-	const PAGE_OVERVIEW = 'cgi-system-info_overview';
+		const WP_ROLE = 'edit_pages';
+		const PAGE_OVERVIEW = 'cgi-system-info_overview';
 
 		public function __construct() {
 
+			register_activation_hook(__FILE__, array($this, 'wp_activate'));
+			register_deactivation_hook(self::PLUGIN, array($this, 'wp_deactivate'));
+
 			add_action('admin_init', array($this, 'wp_admin_init'));
 			add_action('admin_menu', array($this, 'wp_admin_menu'));
+		}
+
+		public function wp_activate() {
+			
+			self::_log(__METHOD__);
+
+			// Nothing done, plugin is activated
+			// return false ;
+
+			// L’extension n’a pu être activée, car elle a déclenché une erreur fatale.
+			//	Fatal error: Uncaught exception 'Exception' with message 'Argh!' 
+			//throw new Exception('Argh!');
+
+			// Page blanche avec le messsage html
+			//wp_die('<p>Argh!<p><ul><li>Revenir plus tard</li><li>Ne jamais revenir</li></ul>');
+		}
+
+		public function wp_deactivate() {
+
+			self::_log(__METHOD__);
+			
 		}
 
 		public function wp_admin_init() {
@@ -49,7 +74,7 @@ if (!class_exists('CgiSystemInfo')) {
 		}
 
 		public function wp_admin_menu() {
-	
+
 			add_menu_page(__('System Info', self::PLUGIN), __('System Info', self::PLUGIN), self::WP_ROLE, self::PAGE_OVERVIEW,
 				array($this, 'wp_on_menu'));
 
@@ -61,15 +86,22 @@ if (!class_exists('CgiSystemInfo')) {
 
 			switch ($_GET['page']) {
 				case self::PAGE_OVERVIEW :
-					
-					include( __DIR__.'/templates/page_overview.php' );
+
+					include( __DIR__ . '/templates/page_overview.php' );
 					break;
 			}
 		}
 
+		public static function _log( $msg )
+		{
+			if(is_array($msg) || is_object($msg))
+				error_log( print_r($msg,true) );
+			else
+				error_log($msg);
+		}
+
 	}
 
-	global $cgiSystemInfo ;
+	global $cgiSystemInfo;
 	$cgiSystemInfo = new CgiSystemInfo();
-
 }
