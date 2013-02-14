63a9f2567b06bb1e49b0640f187eefae1eb640ec
Cyrille37 <cyrille37@gmail.com>
2013-02-14 17:50:51 +0100

    Add some mysql stuff and phpinfo

--- templates/page_overview.php
+++ templates/page_overview.php
@@ -13,9 +13,16 @@ else
 {
 	$curl_version = 'none';
 }
-	
-?>
 
+global $wpdb ;
+
+$mysql_server_version = $wpdb->db_version();
+
+$wp_options_table_fieldnames = $wpdb->get_col('DESC wp_options', 0);
+
+$tables_list = $wpdb->get_col('SHOW TABLES', 0);
+
+?>
 <div class="wrap">
 	<div id="icon-options-general" class="icon32">
 		<br>
@@ -28,17 +35,29 @@ else
 
 	<table class="widefat">
 		<tr>
-			<td nowrap="nowrap">PHP Version</td><td><?php echo PHP_VERSION ?></td>
+			<td nowrap="nowrap">PHP version</td><td><?php echo PHP_VERSION ?></td>
 		</tr>
 		<tr>
-			<td nowrap="nowrap">Wordpress Version</td><td><?php echo $wp_version ?></td>
+			<td nowrap="nowrap">Wordpress version</td><td><?php echo $wp_version ?></td>
+		</tr>
+		<tr>
+			<td nowrap="nowrap">Mysql server version</td><td><?php echo (isset($mysql_server_version) ? $mysql_server_version : 'null') ?></td>
 		</tr>
 		<tr>
 			<td nowrap="nowrap">Curl</td><td><?php echo $curl_version ?></td>
 		</tr>
 		<tr>
-			<td nowrap="nowrap">Hash algos</td><td><?php echo (function_exists('hash_algos')) ? implode(',',hash_algos()): 'none' ?></td>
+			<td nowrap="nowrap">Hash algos</td><td><?php echo (function_exists('hash_algos')) ? implode(',',hash_algos()): 'null' ?></td>
+		</tr>
+		<tr>
+			<td nowrap="nowrap">tables list</td><td><?php echo (is_array($tables_list)) ? implode(',',$tables_list): 'null' ?></td>
+		</tr>
+		<tr>
+			<td nowrap="nowrap">wp_options table fieldnames</td><td><?php echo (is_array($wp_options_table_fieldnames)) ? implode(',',$wp_options_table_fieldnames): 'null' ?></td>
 		</tr>
 	</table>
 
+	<p>&nbsp;</p>
+
+	<?php phpinfo() ?>
 </div>
